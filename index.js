d8599cfc561c52a8fae39698b6e06ec3c6e18658
xauroxs <xauroxs@gmail.com>
2023-10-03 11:08:40 +0300

    add serving for different routes

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// root route
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// a non-root page
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+// a redirect
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+// 1st variant of chaining functions together
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("Attempted to load hello.html");
+
+    next();
+  },
+  (req, res) => {
+    res.send("Hello, World!");
+  }
+);
+
+// 2nd variant of chaining functions together
+const one = (req, res, next) => {
+  console.log("One");
+  next();
+};
+
+const two = (req, res, next) => {
+  console.log("Two");
+  next();
+};
+
+const three = (req, res) => {
+  console.log("Three");
+
+  res.send("Finished!");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
+// every route that is not supported
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(port, () => console.log(`Express app is running on port ${port}`));
